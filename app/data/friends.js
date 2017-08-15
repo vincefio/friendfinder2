b4ebc4a5553a3d4f9dae6a9a532f7b8e241f0036
Vincent Fiorilli <Vincent Fiorilli>
2017-08-15 09:45:45 -0400

    routes working, onto js comparing

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+//below data will hold all of the saved friends
+//it's initially set w dummy data, could be empty as well
+
+var friendList = [
+	{
+		"name": "Keith",
+		"photo": "https://tse1.mm.bing.net/th?id=OIP.FJKEXZHKsGd37Rd_dfkKtwFNC7&pid=15.1&P=0&w=309&h=175",
+		"scores": [
+			"5",
+			"4",
+			"4",
+			"2",
+			"1",
+			"2",
+			"5"
+		]
+
+	}
+]
+
+module.exports = friendList
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,4 +1,20 @@
+//load data from our friends file
+var friendList = require('../data/friends.js')
+
+
+//routing
 module.exports = function(app){
 
-	
+	//get route to show a JSON of all available friends
+	app.get('/api/friends', function(req, res){
+		res.json(friendList)
+	})
+
+	//post route to handle friend input
+	app.post('/api/new', function(req, res){
+		//push new friend to the frienList
+		friendList.push(req.body)
+		res.json(friendList)
+	})
+
 }
